d928f3a50a23fd854e3c80f2d51e5d03631edd41
victo <vicky454ph@gmail.com>
2022-06-01 09:50:06 -0500

    add firebase

--- app/src/main/java/com/example/productsprojectfinal/MainActivity.kt
+++ app/src/main/java/com/example/productsprojectfinal/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.productsprojectfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val database = Firebase.database
+        val myRef  = database.reference
+
+
     }
 }
---
+++ app/src/main/java/com/example/productsprojectfinal/Users.kt
@@ -0,0 +1,13 @@
+package com.example.productsprojectfinal
+
+import java.util.*
+
+class Users(nombre: String, username: String, nivel: Int
+) {
+
+    val nombre = nombre
+    val username = username
+    val nivel = nivel
+
+
+}
